b382a6b7fb6255897f193ca205715ccc785400a4
LolzDEV <lollotorres06@gmail.com>
2020-10-02 16:08:37 +0200

    Added MDEditor module

---
+++ src/modules/mdeditor/windows/WindowMDEditor.vue
@@ -0,0 +1,21 @@
+<template>
+  <WindowIframe :data="data" />
+</template>
+
+<script>
+import WindowIframe from 'C:\\Users\\lollo\\WebstormProjects\\owd-client\\src\\core\\components\\window\\iframe\\WindowIframe.vue'
+
+export default {
+  name: 'WindowMDEditor',
+  components: {WindowIframe},
+  props: {
+    data: Object
+  }
+}
+</script>
+
+<style scoped lang="scss">
+p {
+  color: #AAA;
+}
+</style>
